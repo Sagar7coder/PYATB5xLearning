5ebdb484d0706ad0926fdd9f5d9eb5c97227d110
Sagar7coder <sagaringale.012@gmail.com>
2024-11-11 20:24:08 +0530

    Task 1 and 2

---
+++ nov/ex_07112024_Keywords_Identifier_Variables/Task1_sum_sub_mul_div.py
@@ -0,0 +1,20 @@
+# Task 1
+# Take a 3 input from the user
+# perform the sum, sub, mul and div
+
+# taking user input
+num1 = int(input("Enter number 1 : "))
+num2 = int(input("Enter number 2 : "))
+num3 = int(input("Enter number 3 : "))
+
+# Calculation
+sum = num1 + num2 + num3
+sub = num1 - num2 - num3
+mul = num1 * num2 * num3
+div = num1 / num2 / num3
+
+# printing output
+print("Addition of three numbers", sum)
+print("Subtraction of three number", sub)
+print("Multiplication of three number", mul)
+print("Division of three numbers", div)
---
+++ nov/ex_07112024_Keywords_Identifier_Variables/Task2_quotient_reminder.py
@@ -0,0 +1,17 @@
+# Task 2
+# Take a 2 input from the user Print the Quotient and Remainder
+
+# taking 2 input
+num1 = input("Enter first number : ")
+num2 = input("Enter second number : ")
+
+# convert the input str to int
+num1 = int(num1)
+num2 = int(num2)
+
+Quotient = num1 // num2
+Remainder = num1 % num2
+
+# printing output
+print("The Quotient of number is", Quotient)
+print("The Remainder of number is", Remainder)
